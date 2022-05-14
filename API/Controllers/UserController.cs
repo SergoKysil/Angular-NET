bad13b24b4398241f484b195e31d106173d76cc6
Serhii Kysil <kisluy1910@gmail.com>
2022-05-14 15:37:07 +0300

    feat: add user controller

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers() => await _context.Users.AsNoTracking().ToListAsync();
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> Get(int id) => await _context.Users.FindAsync(id);
+}
